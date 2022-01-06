bf19a95afee15c4fbd7cbe0f7876469b4651a698
Hugo Pinheiro <hugo_hp10@hotmail.com>
2022-01-06 20:18:11 -0300

    first comitt

--- TranslationY Animation/ViewController.swift
+++ TranslationY Animation/ViewController.swift
@@ -11,9 +11,26 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        // ImageView with Image
+        let imageV = UIImageView(frame: CGRect(x: 50, y: 100, width: 150, height: 100))
+        imageV.center = view.center
+        imageV.image = UIImage(named: "devImage", in: Bundle(for: type(of: self)), compatibleWith: nil)
+        view.addSubview(imageV)
+
+        // Scaling
+
+        UIView.animate(withDuration: 1, delay: 0.0, options: [.autoreverse, .repeat], animations: {
+            imageV.transform = CGAffineTransform(translationX: 0, y: 25)
+
+        }) { (complete) in
+            UIView.animate(withDuration: 1, animations: {
+                imageV.transform = CGAffineTransform(translationX: 0, y: 25)
+            })
+            
+            
+        }
+
     }
 
-
 }
-
